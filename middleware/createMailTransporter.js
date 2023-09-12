fef4166e56a5ffb3580e09643609a6aeee381874
vl <goognya@gmail.com>
2023-09-12 21:14:28 +0100

    vars added

--- middleware/createMailTransporter.js
+++ middleware/createMailTransporter.js
@@ -4,15 +4,20 @@ const createMailTransporter = () => {
 
 
     const transporter = nodemailer.createTransport({
-        // service: process.env.EMAIL_DOMEN,
+        // service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_DOMEN,
-        // port: 465,
-        port: 587,
-        // secure: true,
+        port: Number(process.env.EMAIL_PORT),
+        // secure: Boolean(process.env.EMAIL_SECURE),
+        // secureConnection: Boolean(process.env.EMAIL_SECURE_CONNECTION),
+        // debug: Boolean(process.env.EMAIL_DEBUG),
+        // logger: Boolean(process.env.EMAIL_LOGGER),
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        // tls: {
+        //     rejectUnauthorized: Boolean(process.env.EMAIL_UNAUTHORIZED)
+        // }
     })
 
     return transporter
