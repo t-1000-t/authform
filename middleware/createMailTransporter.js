b25a33ed096cfd1af62b5b3a0073daca8b4ca697
vl <goognya@gmail.com>
2023-09-15 13:25:26 +0100

    add new vars in heroku

--- middleware/createMailTransporter.js
+++ middleware/createMailTransporter.js
@@ -2,8 +2,7 @@ const nodemailer = require('nodemailer')
 
 const createMailTransporter = () => {
 
-
-    const transporter = nodemailer.createTransport({
+    const emailHost = {
         // service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_DOMEN,
         port: Number(process.env.EMAIL_PORT),
@@ -18,7 +17,26 @@ const createMailTransporter = () => {
         // tls: {
         //     rejectUnauthorized: Boolean(process.env.EMAIL_UNAUTHORIZED)
         // }
-    })
+    }
+
+    const emailGmailHost = {
+        host: process.env.EMAIL_DOMEN_GMAIL,
+        // host: process.env.EMAIL_DOMEN,
+        port: Number(process.env.EMAIL_PORT_GMAIL),
+        secure: Boolean(process.env.EMAIL_SECURE),
+            // secureConnection: Boolean(process.env.EMAIL_SECURE_CONNECTION),
+            // debug: Boolean(process.env.EMAIL_DEBUG),
+            // logger: Boolean(process.env.EMAIL_LOGGER),
+        auth: {
+            user: process.env.EMAIL_USER_GMAIL,
+            pass: process.env.EMAIL_PASS_GMAIL
+        },
+    }
+
+    console.log('email user', process.env.EMAIL_USER)
+    console.log('email has "gmail"', process.env.EMAIL_USER.includes('gmail'))
+
+    const transporter = nodemailer.createTransport(emailHost)
 
     return transporter
 }
