6b3e453ec8107ae8485d549e03e236d4305ffe42
vl <goognya@gmail.com>
2024-02-20 14:28:17 +0000

    fixed 2

--- src/service/generateCustomId.js
+++ src/service/generateCustomId.js
@@ -1,14 +1,14 @@
 const generateCustomId = () => {
-    let id = '';
-    const characters = 'abcdef0123456789';
-    const length = 20; // Adjust the length as needed
+    let id = ''
+    const characters = 'abcdef0123456789'
+    const length = 20  // Adjust the length as needed
 
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        id += characters[randomIndex];
+        const randomIndex = Math.floor(Math.random() * characters.length)
+        id += characters[randomIndex]
     }
 
-    return id;
+    return id
 }
 
 module.exports = generateCustomId
