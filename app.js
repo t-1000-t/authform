19e2a80face40090f63948987fd028e6d7dd5fd8
vl <goognya@gmail.com>
2024-06-01 16:22:48 +0100

    Add socket

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const passport = require('passport')
+const http = require('http')
 const cors = require('cors')
 const app = express()
+const server = http.createServer(app)
 const config = require('./config/config')
 const apiRouter = require('./router/router')
 
-app.use(cors('*'))
+app.use(cors({
+  origin: process.env.CLIENT_FRONT_URL,
+  methods: ["GET", "POST"],
+}))
 
 if (config.mode === 'development') {
   const logger = require('morgan')
@@ -27,4 +32,33 @@ app.get('/', (req, res) => {
 
 app.use('/api', apiRouter)
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}`))
+const io = require('socket.io')(server, {
+  cors: {
+    origin: `${process.env.CLIENT_FRONT_URL}`,
+    methods: [ "GET", "POST" ]
+  },
+  pingInterval: 2000,
+  pingTimeout: 5000,
+})
+
+io.on('connection', (socket) => {
+  console.log("User connected", socket.id);
+  socket.emit("me", socket.id);
+
+  socket.on("callUser", (data) => {
+    console.log('Call User', data.userToCall);
+    io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+    socket.broadcast.emit("callEnded");
+  });
+});
+
+
+server.listen(config.port, () => console.log(`Server running on port ${config.port}`))
