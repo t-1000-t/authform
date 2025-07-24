112eb43f5f39ae18195d0c30a2c30006c508cadb
vl <goognya@gmail.com>
2025-07-24 18:33:50 +0100

    I've fixed Cvbody

--- src/models/CVbody.js
+++ src/models/CVbody.js
@@ -1,21 +1,24 @@
+const { language } = require('googleapis/build/src/apis/language')
 const mongoose = require('mongoose')
 
 const cvBodySchema = new mongoose.Schema(
     {
-        fullname: {
-           type: String,
-           require: true,
-        },
-        info: {
-                email: String,
-                linked: String,
-                local: String,
-                lang: String,
-                img: String
-        },
         title: {
-            type: String,
-            require: true
+            fullname: {
+                type: String,
+                require: true,
+            },
+            posname: {
+                type: String,
+                require: true
+            }
+        },
+        contacts: {
+                email: String,
+                linkedin: String,
+                location: String,
+                language: String,
+                img: String
         }
     }
 )
