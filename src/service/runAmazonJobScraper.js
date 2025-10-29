1ca1270b1c9578db1a5a1429ce901b8d8cb6cc1f
vl <goognya@gmail.com>
2025-10-29 13:08:00 +0000

    Ivw made a fix schedule

--- src/service/runAmazonJobScraper.js
+++ src/service/runAmazonJobScraper.js
@@ -17,10 +17,20 @@ function runAmazonJobScraper() {
     }
 
     // Every 30 minutes from 07:00–19:30
-    const t1 = cron.schedule('0,30 7-19 * * *', run, { timezone })
+
+    // -Runs exactly twice per hour:
+    // -at minute 0 (e.g. 07:00, 08:00, 09:00, …)
+    // -and minute 30 (e.g. 07:30, 08:30, 09:30, …)
+    // const t1 = cron.schedule('0,30 7-19 * * *', run, { timezone })
+
+    // -'*/30 7-19 * * *'
+    // -Uses a step value, meaning “every 30 minutes starting at 0.”
+    // -It’s more flexible and semantic — if the system ever starts at a non-zero minute, it still aligns to every 30-minute interval (00, 30, 00, 30…).
+    // -Easier to adjust — e.g. change to */15 for every 15 minutes.
+    // const t1 = cron.schedule('*/30 7-19 * * *', run, { timezone })
 
     // Every 60 minutes from 07:00–19:00
-    // const t1 = cron.schedule('0,30 7-19 * * *', run, { timezone })
+    const t1 = cron.schedule('0 7-19 * * *', run, { timezone })
 
     // One last run at 20:00
     const t2 = cron.schedule('0 20 * * *', run, { timezone })
